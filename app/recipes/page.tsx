cfe912922e9b585a083838130e2e7b7fb5bf6467
gannont14 <tubbsgannon14@gmail.com>
2025-01-30 14:58:20 -0500

    updated all recipes page to show cards of all recipes from every user

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,12 +1,26 @@
 import { getAllRecipes } from '@/lib/repositories/recipeRepository'
+import RecipeCard from '@/components/recipes/recipe-card'
+import { Recipe } from '@/types/recipes/recipes'
 
 export default async function RecipesPage() {
-  const recipes = await getAllRecipes()
+  const recipes: Recipe[] = await getAllRecipes()
+
+  if(!recipes){
+    return (<div> No recipes available </div>);
+  }
+
 
   return (
     <div className="max-w-full mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">All Recipes</h1>
-      <pre>{JSON.stringify(recipes, null, 2)}</pre>
+      <h1 className="text-3xl font-bold mb-6">My Recipes</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {recipes.map((recipe: Recipe) => (
+          <RecipeCard
+            recipe={recipe}
+            cardKey={recipe.id}
+          />
+        ))}
+      </div>
     </div>
   )
 }
